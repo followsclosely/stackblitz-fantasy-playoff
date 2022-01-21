13359e3541c7f03d68742c452a523807d1e905b3
followsclosely <mlavwilson@gmail.com>
2022-01-21 11:48:24 -0500

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,24 @@ export class AppComponent  {
 
   name = 'Angular ' + VERSION.major;
 
+  public roster : Array<Player> = [];
   public playerPool : Map<string, Array<Player>>;
   
   public positions : Array<string> = ["QB","RB","WR","TE","K","D"];
 
   constructor(private playerService: PlayerPoolService){
     this.playerPool = playerService.getPlayerPool();
+  }
+
+  toggle(player : Player){
+    
+    var index = this.roster.indexOf(player);
+    if( index >= 0) {
+      console.log("Removing " + player);
+      delete this.roster[index];
+    } else {
+      console.log("Adding " + player);
+      this.roster.push(player);
+    }
   }
 }
